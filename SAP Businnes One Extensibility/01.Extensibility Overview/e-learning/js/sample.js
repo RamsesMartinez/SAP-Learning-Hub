bb0bf83222083a8d13aeac55e6448773aee2db7d
Ramses <ramses.mtz96@gmail.com>
2017-12-27 16:49:31 -0600

    feature: Add jquery file
    add new functions to sample.js

--- SAP Businnes One Extensibility/01.Extensibility Overview/e-learning/js/sample.js
+++ SAP Businnes One Extensibility/01.Extensibility Overview/e-learning/js/sample.js
@@ -1,12 +1,25 @@
-// new element
+$(document).ready(function() {
+  $('#btn1').click(function() {
+    createContent();
+  });
+  
+  $('#btn2').click(function() {
+    $("h1").fadeOut();
+  });
+});
 
-var newHeading = document.createElement('h1');
-var newParagraph = document.createElement('p');
 
-// to add content
-newHeading.innerHTML = 'Di you now know?';
-newParagraph.innerHTML = 'Most kangaroos are left-handed';
-
-// add new elements to the html
-document.getElementById('jsDiv').appendChild(newHeading);
-document.getElementById('jsDiv').appendChild(newParagraph);
+function createContent(){
+  // new element
+  var newHeading = document.createElement('h1');
+  var newParagraph = document.createElement('p');
+  
+  // to add content
+  newHeading.innerHTML = 'Di you now know?';
+  newParagraph.innerHTML = 'Most kangaroos are left-handed';
+  
+  // add new elements to the html
+  document.getElementById('jsDiv').appendChild(newHeading);
+  document.getElementById('jsDiv').appendChild(newParagraph);
+  
+}
